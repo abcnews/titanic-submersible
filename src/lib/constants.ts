c0daa5d702b8184b723db6f3c4f036563ab00711
Simon Elvery <simon@elvery.net>
2023-06-23 11:49:08 +1000

    Accommodate larger font sizes

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,6 +1,11 @@
+import { getDefaultFontSize } from './utils';
+
+const DEFAULT_FONT_SIZE = getDefaultFontSize() || 16;
 export const TITANIC = 3800;
-export const SCREENS = 30;
+export const SCREENS = (30 * DEFAULT_FONT_SIZE) / 16;
 export const COLUMNS = 10;
 export const ROWS = 10;
 export const ROWS_PER_METRE = (SCREENS * ROWS) / TITANIC;
 export const METRES_PER_ROW = TITANIC / (SCREENS * ROWS);
+
+console.log('SCREENS :>> ', SCREENS);
